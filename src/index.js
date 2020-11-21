4ff1f15368accb8ae02be7001af1e8100427b013
TomozQ <63640328+TomozQ@users.noreply.github.com>
2020-11-21 20:39:06 +0900

    jsbasic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import "./styles.css";
+// const, let等の変数宣言
+// let val1 = 'let変数'
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// val1 = 'let変数を上書き';
+// console.log(val1);
+
+//letは再宣言不可能
+// let val1 = 'let変数を再宣言';
+
+// const val2 = 'const変数';
+// console.log(val2);
+
+// val2 = 'const変数を上書き'
+
+// const val2 = 'constを再宣言';
+// console.log(val2);
+
+//constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: 'aaa',
+//   age: '28',
+// }
+// console.log(val4);
+
+// val4.name = 'jakee';
+// val4.address = 'hiroshima';
+// console.log(val4);
+
+const val5 = ["dog", "cat"];
+val5[0] = "bard";
+val5.push("monkey");
+console.log(val5);
